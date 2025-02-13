d8020ab110ab7b580d0b99b5924870ddb4f9e79f
Viral Haria <viralharia97@gmail.com>
2025-02-12 22:54:41 -0800

    Updated Player component to consume and display player name entered by the user

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,19 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerName = useRef();
+  const [playerNameInput, setName] = useState(null);
+
+  function handleClick() {
+    setName(playerName.current.value);
+    playerName.current.value = "";
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerNameInput ?? "unknown player"}!</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={playerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
